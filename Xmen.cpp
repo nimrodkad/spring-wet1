57b25d30ca57300a238bec6544905e3e901317f7
nimrodkad <nimrodk@campus.technion.ac.il>
2017-05-11 22:58:54 +0300

    Updates

--- Xmen.cpp
+++ Xmen.cpp
@@ -45,23 +45,21 @@ bool Xmen::addStudent(int studentID, int grade, int power) {
 	this->updateMostPowerful();
 	return true;
 }
+
 //return pointers to the student in the id tree and in the power tree(2th param)
 Student *Xmen::findStudent(int studentID, Student **studentOut) {
 	Student *dummyStudent1 = new Student(studentID, 0, 0);
 	AVLtree<Student, Student, compByStudentID>::AVLNode *lastNode1;
 	AVLtree<Student, Student, compByStudentID>::AVLNode *studentById =
-			this->students->find(this->students->root, dummyStudent1,
-					&lastNode1);
+			this->students->find(this->students->root, dummyStudent1, &lastNode1);
 	delete dummyStudent1;
 	if (!studentById) {
 		return NULL;
 	}
-	Student *dummyStudent2 = new Student(studentID, studentById->keyValue->PWR,
-			0);
+	Student *dummyStudent2 = new Student(studentID, studentById->keyValue->grade, studentById->keyValue->PWR);
 	AVLtree<Student, Student, compByStudentPower>::AVLNode *lastNode2;
 	AVLtree<Student, Student, compByStudentPower>::AVLNode *studentByPwr =
-			this->studentsPowers->find(this->studentsPowers->root,
-					dummyStudent2, &lastNode2);
+			this->studentsPowers->find(this->studentsPowers->root, dummyStudent2, &lastNode2);
 	delete dummyStudent2;
 	if (!studentByPwr) {
 		return NULL;
@@ -73,8 +71,7 @@ Student *Xmen::findStudent(int studentID, Student **studentOut) {
 Team *Xmen::findTeam(int teamID) {
 	Team *newTeam = new Team(teamID);
 	AVLtree<Team, Team, compByTeamID>::AVLNode *lastNode;
-	AVLtree<Team, Team, compByTeamID>::AVLNode *teamNode = this->teams->find(
-			this->teams->root, newTeam, &lastNode);
+	AVLtree<Team, Team, compByTeamID>::AVLNode *teamNode = this->teams->find(this->teams->root, newTeam, &lastNode);
 	delete newTeam;
 	if (!teamNode) {
 		return NULL;
